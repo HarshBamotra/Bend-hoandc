be30c213586e8fc6a8e72dd108594244453ee65b
harshbamotra <harshbamotra12@gmail.com>
2022-04-27 21:30:25 +0530

    Defined put methods

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ app.post('/student', (req, res) =>{
     const { twitterHandle } = req.body;
     const { linkedinHandle } = req.body;
     const { examsCleared } = req.body;
+    const { password } = req.body;
 
     var name = firstName + " " + lastName;
     var contact = twitterHandle + "," + linkedinHandle;
@@ -76,7 +77,7 @@ app.post('/student', (req, res) =>{
 
     //query to insert data into student table 
     connection.query(`INSERT INTO STUDENT VALUES('${name}' , '${mobile}', '${course}', '${yearOfGraduation}', '${email}', 
-    ${HE}, ${job}, '${contact}', '${exams}');`, function(error){
+    ${HE}, ${job}, '${contact}', '${exams}' , '${password}');`, function(error){
         if(!!error){
             console.log("Error(Student):"+error.message);
         }
@@ -164,6 +165,29 @@ app.get('/student/email/:email', (req, res)=>{
         else if(row.length <= 0){
             res.send({
                 Message:`Entry with ID ${email} not found !!`,
+            });
+        }
+        else{
+            res.status(200).send({
+                row
+            })
+        }
+    })
+});
+
+//for getting password of an alumni by email ID
+app.get('/student/password/:email', (req, res)=>{
+    const { email } = req.params;
+
+    connection.query(`SELECT PASSWORD FROM STUDENT WHERE EMAIL = '${email}';`, function(error, row){
+        if(!!error){
+            res.send({
+                Message:"Error : " + error.message,
+            });
+        }
+        else if(row.length <= 0){
+            res.send({
+                Message:`No entries with course ${Cname} found !!`,
             });
         }
         else{
@@ -351,11 +375,14 @@ app.delete('/student/:email', (req, res)=>{
 
 /* ----------------- API's Update methods starts from here ----------------- */
 
-app.put('/student/email/:email', (req, res)=>{
+/* ----------------- For student table ----------------- */
+app.put('/student/name/:email', (req, res)=>{
     const { email } = req.params;
-    const { Name } = req.body;
+    const { firstName } = req.body;
+    const { lastName } = req.body;
+    var Name = firstName + " " + lastName;
 
-    connection.query(`UPDATE TABLE STUDENT SET NAME = "${ Name }" where EMAIL = "${ email }"`, 
+    connection.query(`UPDATE STUDENT SET NAME = '${ Name }' where EMAIL = '${ email }';`, 
     function(error){
         if(!!error){
             res.send({
@@ -365,6 +392,371 @@ app.put('/student/email/:email', (req, res)=>{
         else{
             res.send({
                 Message:`Entry with ${ email } updated with name ${ Name } sucessfully !!`,
+            });
+        }
+    })
+});
+
+
+app.put('/student/mobile/:email', (req, res)=>{
+    const { email } = req.params;
+    const { mobile } = req.body;
+
+    connection.query(`UPDATE STUDENT SET MOBILE_NO = '${ mobile }' where EMAIL = '${ email }';`, 
+    function(error){
+        if(!!error){
+            res.send({
+                Message:"Error ::" + error.message,
+            });
+        }
+        else{
+            res.send({
+                Message:`Entry with ${ email } updated sucessfully !!`,
+            });
+        }
+    })
+});
+
+
+app.put('/student/course/:email', (req, res)=>{
+    const { email } = req.params;
+    const { course } = req.body;
+
+    connection.query(`UPDATE STUDENT SET COURSE = '${ course }' where EMAIL = '${ email }';`, 
+    function(error){
+        if(!!error){
+            res.send({
+                Message:"Error ::" + error.message,
+            });
+        }
+        else{
+            res.send({
+                Message:`Entry with ${ email } updated sucessfully !!`,
+            });
+        }
+    })
+});
+
+
+app.put('/student/yearOfGraduation/:email', (req, res)=>{
+    const { email } = req.params;
+    const { yearOfGraduation } = req.body;
+
+    connection.query(`UPDATE STUDENT SET YOG = '${ yearOfGraduation }' where EMAIL = '${ email }';`, 
+    function(error){
+        if(!!error){
+            res.send({
+                Message:"Error ::" + error.message,
+            });
+        }
+        else{
+            res.send({
+                Message:`Entry with ${ email } updated sucessfully !!`,
+            });
+        }
+    })
+});
+
+
+app.put('/student/higherEducation/:email', (req, res)=>{
+    const { email } = req.params;
+    const { higherEducation } = req.body;
+
+    connection.query(`UPDATE STUDENT SET HIGHER_EDUCATION = '${ higherEducation }' where EMAIL = '${ email }';`, 
+    function(error){
+        if(!!error){
+            res.send({
+                Message:"Error ::" + error.message,
+            });
+        }
+        else{
+            res.send({
+                Message:`Entry with ${ email } updated sucessfully !!`,
+            });
+        }
+    })
+});
+
+
+app.put('/student/workExperience/:email', (req, res)=>{
+    const { email } = req.params;
+    const { workExperience } = req.body;
+
+    connection.query(`UPDATE STUDENT SET JOB = '${ workExperience }' where EMAIL = '${ email }';`, 
+    function(error){
+        if(!!error){
+            res.send({
+                Message:"Error ::" + error.message,
+            });
+        }
+        else{
+            res.send({
+                Message:`Entry with ${ email } updated sucessfully !!`,
+            });
+        }
+    })
+});
+
+
+app.put('/student/contact/:email', (req, res)=>{
+    const { email } = req.params;
+    const { twitterHandle } = req.body;
+    const { linkedinHandle } = req.body;
+    var contact = twitterHandle + "," + linkedinHandle;
+
+    connection.query(`UPDATE STUDENT SET CONTACT = '${ contact }' where EMAIL = '${ email }';`, 
+    function(error){
+        if(!!error){
+            res.send({
+                Message:"Error ::" + error.message,
+            });
+        }
+        else{
+            res.send({
+                Message:`Entry with ${ email } updated sucessfully !!`,
+            });
+        }
+    })
+});
+
+
+app.put('/student/examsCleared/:email', (req, res)=>{
+    const { email } = req.params;
+    const { examsCleared } = req.body;
+
+    connection.query(`UPDATE STUDENT SET EXAMS_QUALIFIED = '${ examsCleared }' where EMAIL = '${ email }';`, 
+    function(error){
+        if(!!error){
+            res.send({
+                Message:"Error ::" + error.message,
+            });
+        }
+        else{
+            res.send({
+                Message:`Entry with ${ email } updated sucessfully !!`,
+            });
+        }
+    })
+});
+
+
+app.put('/student/password/:email', (req, res)=>{
+    const { email } = req.params;
+    const { password } = req.body;
+
+    connection.query(`UPDATE STUDENT SET PASSWORD = '${ password }' where EMAIL = '${ email }';`, 
+    function(error){
+        if(!!error){
+            res.send({
+                Message:"Error ::" + error.message,
+            });
+        }
+        else{
+            res.send({
+                Message:`Entry with ${ email } updated sucessfully !!`,
+            });
+        }
+    })
+});
+
+
+// app.put('/student/email/:email', (req, res)=>{
+//     const { email } = req.params;
+//     const { Email } = req.body;
+//     var HE, job;            
+    
+//     connection.query(`UPDATE STUDENT SET EMAIL = '${Email}' WHERE EMAIL = '${ email }';`, function(error){
+//         if(!!error){
+//             console.log("Error(Student): " + error.message);
+//             res.send({
+//                 Message:"Some error occured !! please check console for more details.",
+//             });
+//         }
+//         else{
+//             res.send({
+//                 Message:`Entry with email ${email} updated succesfully !!`,
+//             });
+//         }
+//     })
+
+//     connection.query(`SELECT Higher_Education, Job FROM STUDENT WHERE EMAIL = '${email}';`, function(error, row){
+//         if(!!error){
+//             console.log("Error : " + error.message);
+//         }
+//         else if(row.length <= 0){
+//             console.log(`Entry with ID ${email} not found !!`);
+//             res.send({
+//                 Message:`Entry with ID ${email} not found !!`,
+//             });
+//         }
+//         else{
+//             HE = row[0].Higher_Education;
+//             job = row[0].Job;
+
+//             if(HE == 1){
+//                 connection.query(`UPDATE HIGHER_EDU SET EMAIL = '${Email}' WHERE EMAIL = '${ email }';`, function(error){
+//                     if(!!error){
+//                         console.log("Error(Higher_edu): " + error.message);
+//                     }
+//                     else{
+//                         console.log(`Entry with email ${email} update from higher_edu succesfully !!`);
+//                     }
+//                 })
+//             }
+        
+//             if(job == 1){
+//                 connection.query(`UPDATE WORK SET EMAIL = '${Email}' WHERE EMAIL = '${ email }';`, function(error){
+//                     if(!!error){
+//                         console.log("Error(Work): " + error.message);
+//                     }
+//                     else{
+//                         console.log(`Entry with email ${email} update from work succesfully !!`);
+//                     }
+//                 })
+//             }
+//         }
+//     })
+// });
+
+/* ----------------- For work table ----------------- */
+
+app.put('/work/type/:email', (req, res)=>{
+    const { email } = req.params;
+    const { type } = req.body;
+
+    connection.query(`UPDATE WORK SET TYPE = '${ type }' where EMAIL = '${ email }';`, 
+    function(error){
+        if(!!error){
+            res.send({
+                Message:"Error ::" + error.message,
+            });
+        }
+        else{
+            res.send({
+                Message:`Entry with ${ email } updated sucessfully !!`,
+            });
+        }
+    })
+});
+
+
+app.put('/work/experience/:email', (req, res)=>{
+    const { email } = req.params;
+    const { experience } = req.body;
+
+    connection.query(`UPDATE WORK SET Experience = '${ experience }' where EMAIL = '${ email }';`, 
+    function(error){
+        if(!!error){
+            res.send({
+                Message:"Error ::" + error.message,
+            });
+        }
+        else{
+            res.send({
+                Message:`Entry with ${ email } updated sucessfully !!`,
+            });
+        }
+    })
+});
+
+
+app.put('/work/organisation/:email', (req, res)=>{
+    const { email } = req.params;
+    const { organisation } = req.body;
+
+    connection.query(`UPDATE WORK SET organisation = '${ organisation }' where EMAIL = '${ email }';`, 
+    function(error){
+        if(!!error){
+            res.send({
+                Message:"Error ::" + error.message,
+            });
+        }
+        else{
+            res.send({
+                Message:`Entry with ${ email } updated sucessfully !!`,
+            });
+        }
+    })
+});
+
+
+app.put('/work/profile/:email', (req, res)=>{
+    const { email } = req.params;
+    const { profile } = req.body;
+
+    connection.query(`UPDATE WORK SET profile = '${ profile }' where EMAIL = '${ email }';`, 
+    function(error){
+        if(!!error){
+            res.send({
+                Message:"Error ::" + error.message,
+            });
+        }
+        else{
+            res.send({
+                Message:`Entry with ${ email } updated sucessfully !!`,
+            });
+        }
+    })
+});
+
+
+/* ----------------- For work table ----------------- */
+
+app.put('/higher_edu/higher_edu/:email', (req, res)=>{
+    const { email } = req.params;
+    const { higher_edu } = req.body;
+    const { Nhigher_edu } = req.body;
+
+    connection.query(`UPDATE HIGHER_EDU SET HIGHER_EDU = '${ Nhigher_edu }' where EMAIL = '${ email }' AND HIGHER_EDU = '${ higher_edu }';`, 
+    function(error){
+        if(!!error){
+            res.send({
+                Message:"Error ::" + error.message,
+            });
+        }
+        else{
+            res.send({
+                Message:`Entry with ${ email } updated sucessfully !!`,
+            });
+        }
+    })
+});
+
+app.put('/higher_edu/yearOfGraduation/:email', (req, res)=>{
+    const { email } = req.params;
+    const { higher_edu } = req.body;
+    const { yearOfGraduation } = req.body;
+
+    connection.query(`UPDATE HIGHER_EDU SET YOG = '${ yearOfGraduation }' where EMAIL = '${ email }' AND HIGHER_EDU = '${ higher_edu }';`, 
+    function(error){
+        if(!!error){
+            res.send({
+                Message:"Error ::" + error.message,
+            });
+        }
+        else{
+            res.send({
+                Message:`Entry with ${ email } updated sucessfully !!`,
+            });
+        }
+    })
+});
+
+app.put('/higher_edu/institute/:email', (req, res)=>{
+    const { email } = req.params;
+    const { higher_edu } = req.body;
+    const { institute } = req.body;
+
+    connection.query(`UPDATE HIGHER_EDU SET Institute = '${ institute }' where EMAIL = '${ email }' AND HIGHER_EDU = '${ higher_edu }';`, 
+    function(error){
+        if(!!error){
+            res.send({
+                Message:"Error ::" + error.message,
+            });
+        }
+        else{
+            res.send({
+                Message:`Entry with ${ email } updated sucessfully !!`,
             });
         }
     })
